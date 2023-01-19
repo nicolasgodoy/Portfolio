1f7bb13f05156710b2be590ec6288eb36407cb84
nicolasgodoy <nicolasgodoy540@gmail.com>
2023-01-19 05:46:27 -0300

    Nuevo disenio

--- src/Components/Componente-1/Mi_Educacion.js
+++ src/Components/Componente-1/Mi_Educacion.js
@@ -23,45 +23,45 @@ const Mi_Educacion = () => {
       <h3 className="col-md-12 Titulo_Educacion">Arbol Academico</h3>
         
 
-        <p>2020 - 2023 Formacion Autodidacta <i class="Icono-html fab fa-html5"></i> <i class=" Icono-Css3 fab fa-css3-alt"></i> <i class=" Icono-Js fab fa-js-square"></i> <i class=" Icono-Code fas fa-code"></i> <i class=" Icono-git fas fa-code-branch"></i> </p>  
-        <div class="contenido"></div>
+        <p>2020 - 2023 Formacion Autodidacta <i className="Icono-html fab fa-html5"></i> <i className=" Icono-Css3 fab fa-css3-alt"></i> <i className=" Icono-Js fab fa-js-square"></i> <i className=" Icono-Code fas fa-code"></i> <i className=" Icono-git fas fa-code-branch"></i> </p>  
+        <div className="contenido"></div>
       
-        <div class="contenido1"></div>
+        <div className="contenido1"></div>
 
-        <div class="contenido2"></div>
+        <div className="contenido2"></div>
 
-        <p>22/03/2021 - 22/5/2021 - Certificado Responsive HTML5 y CSS3 <i class="Icono-html fab fa-html5"></i> <i class=" Icono-Css3 fab fa-css3-alt"></i>  </p> 
+        <p>22/03/2021 - 22/5/2021 - Certificado Responsive HTML5 y CSS3 <i className="Icono-html fab fa-html5"></i> <i className=" Icono-Css3 fab fa-css3-alt"></i>  </p> 
 
-        <div class="contenido3"></div>
+        <div className="contenido3"></div>
 
-        <div class="contenido4"></div>
+        <div className="contenido4"></div>
 
-        <div class="contenido5"></div>
+        <div className="contenido5"></div>
       
-        <p> 22/06/2021 - 24/08/2021 - Certificado de Programacion web con Javascript <i class=" Icono-Js fab fa-js-square"></i> </p>  
+        <p> 22/06/2021 - 24/08/2021 - Certificado de Programacion web con Javascript <i className=" Icono-Js fab fa-js-square"></i> </p>  
 
-        <div class="contenido6"></div>
+        <div className="contenido6"></div>
 
-        <div class="contenido7"></div>
+        <div className="contenido7"></div>
 
-        <div class="contenido8"></div>
+        <div className="contenido8"></div>
 
         <p> 24/08/2022 - 04/10/2022 - Certificado de .NET <img className="Icono-Csharp" src={imgNET}></img> <img className="Icono-Csharp" src={imgCsharp}></img></p>  
      
-        <div class="contenido6"></div>
+        <div className="contenido6"></div>
 
-        <div class="contenido7"></div>
+        <div className="contenido7"></div>
 
-        <div class="contenido8"></div>
+        <div className="contenido8"></div>
 
         <p> 14/10/2022 - 14/12/2022 - Diplomatura de .NET <img className="Icono-Csharp" src={imgNET}></img> <img className="Icono-Csharp" src={imgCsharp}></img></p>    
 
-        <div class="contenido6"></div>
+        <div className="contenido6"></div>
 
-        <div class="contenido7"></div>
+        <div className="contenido7"></div>
 
-        <div class="contenido8"></div>
-        <p>2020 - 2023 Facultad - A.Sistemas -  A Finalizar <i class=" Icono-libro fas fa-book"></i> <i class=" Icono-taza fas fa-coffee"></i> </p>
+        <div className="contenido8"></div>
+        <p>2020 - 2023 Facultad - A.Sistemas -  A Finalizar <i className=" Icono-libro fas fa-book"></i> <i className=" Icono-taza fas fa-coffee"></i> </p>
         
     </div>
 
