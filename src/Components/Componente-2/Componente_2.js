5dd0f91381c0fa07820c3292defd51bb0a6a56ee
nicolasgodoy <nicolasgodoy540@gmail.com>
2024-05-12 18:57:08 -0300

    subiendo cambios

--- src/Components/Componente-2/Componente_2.js
+++ src/Components/Componente-2/Componente_2.js
@@ -10,92 +10,68 @@ import IconoAngular from '../Componente-2/iconangular.png';
 import IconoCSharp from '../Componente-2/Microsoft_.NET_logo.png';
 import IconoAspNet from '../Componente-2/asp_net.png';
 import IconoSQLServer from '../Componente-2/imagenSQl.png';
-import IconoMySql from '../Componente-2/mysql3.png';
-import IconoNodeJs from '../Componente-2/nodejs.png';
+import IconoReact from '../Componente-2/iconreact.png';
 import imagen from '../Componente-2/undraw_certificate_re_yadi.svg';
 
 
 class Componente_2 extends Component {
-
     render() {
-
-        return <div className="Componente_2 container animate__animated animate__fadeIn" id="Skills">
-
-
+        return (
+            <div className="Componente_2 container-fluid animate__animated animate__fadeIn" id="Skills">
             <h1 className="Titulo_Leng">Habilidades</h1>
-
             <div className="Contenido_Componente_2">
-
-                <div className="Contenido_Leng col-md-2">
-
-                    <img className="Icono-html" src={IconoHtml} />
-                    <p className="card-title">HTML-5</p>
-
-                </div>
-
-                <div className=" Contenido_Leng col-md-2">
-                    <img className="Icono-Css3" src={IconoCss3} />
-                    <p className="card-title "> CSS-3 </p>
-
-                </div>
-
-                <div className=" Contenido_Leng col-md-2">
-                    <img className="Icono-Js" src={IconoJs} />
-                    <p className="card-title"> Javascript </p>
-
-                </div>
-
-                <div className="Contenido_Leng col-md-2">
-                    <img className="Icono-React" src={IconoTelerik} />
-                    <p className="card-title "> Telerik </p>
-
-                </div>
-
-                <div className="Contenido_Leng col-md-2">
-                    <img className="Icono-React" src={IconoAngular} />
-                    <p className="card-title "> Angular </p>
-
-                </div>
-
-                <div className="Contenido_Leng col-md-2">
-                    <img className="Icono-GitHub" src={IconoGitHub} />
-                    <p className="card-title "> GitHub </p>
-
-                </div>
-
-                <div className="Contenido_Leng col-md-2">
-                    <img className="Icono-Boostrap" src={IconoBoostrap} />
-                    <p className="card-title "> Boostrap </p>
-
-                </div>
-
-                <div className="Contenido_Leng col-md-2">
-                    <img className="Icono-NET" src={IconoCSharp} />
-                    <p class="card-title"> .NET </p>
-
-                </div>
-
-                <div className="Contenido_Leng col-md-2">
-                    <img className="Icono-NET" src={IconoAspNet} />
-                    <p class="card-title"> ASP.NET </p>
-
-                </div>
-
-                <div className="Contenido_Leng col-md-2">
-                    <img className="Icono-SqlServer" src={IconoSQLServer} />
-                    <p class="card-title"> SQLServer </p>
-
-                </div>
-
+              <div className="Contenido_Leng col-md-3">
+                <img className="Icono-html" src={IconoHtml} />
+                <p className="card-title">HTML-5</p>
+              </div>
+              <div className="Contenido_Leng col-md-3">
+                <img className="Icono-Css3" src={IconoCss3} />
+                <p className="card-title "> CSS-3 </p>
+              </div>
+              <div className=" Contenido_Leng col-md-3">
+                <img className="Icono-Js" src={IconoJs} />
+                <p className="card-title"> Javascript </p>
+              </div>
+              <div className="Contenido_Leng col-md-3">
+                <img className="Icono-React" src={IconoTelerik} />
+                <p className="card-title "> Telerik </p>
+              </div>
+              <div className="Contenido_Leng col-md-3">
+                <img className="Icono-React" src={IconoAngular} />
+                <p className="card-title "> Angular </p>
+              </div>
+              <div className="Contenido_Leng col-md-3">
+                <img className="Icono-React" src={IconoReact} />
+                <p className="card-title "> React </p>
+              </div>
             </div>
-
+            <div className="Contenido_Componente_2">
+              <div className="Contenido_Leng col-md-3">
+                <img className="Icono-GitHub" src={IconoGitHub} />
+                <p className="card-title "> GitHub </p>
+              </div>
+              <div className="Contenido_Leng col-md-3">
+                <img className="Icono-Boostrap" src={IconoBoostrap} />
+                <p className="card-title "> Boostrap </p>
+              </div>
+              <div className="Contenido_Leng col-md-3">
+                <img className="Icono-NET" src={IconoCSharp} />
+                <p className="card-title"> .NET </p>
+              </div>
+              <div className="Contenido_Leng col-md-3">
+                <img className="Icono-NET" src={IconoAspNet} />
+                <p className="card-title"> ASP.NET </p>
+              </div>
+              <div className="Contenido_Leng col-md-3">
+                <img className="Icono-SqlServer" src={IconoSQLServer} />
+                <p className="card-title"> SQLServer </p>
+              </div>
+            </div>
             <div className="col-md-12 animate__animated animate__fadeInUp">
-                    <img className="imagen_skills" src={imagen} />
+              <img className="imagen_skills" src={imagen} />
             </div>
-        </div>
-
+          </div>
+        );
     }
-
 }
-
 export default Componente_2;
